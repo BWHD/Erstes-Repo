13450ab1fa8754a893b750a8600b2329d2c3565c
Bastian W <bastianwilkat@Bastians-MBP.fritz.box>
2018-11-20 13:00:38 +0100

    Dateikopie erstellt

---
+++ Lernen/funktion Kopie.py
@@ -0,0 +1,19 @@
+class Auto:
+    anzahl_räder = 0
+    farbe = ""
+    hersteller = ""
+
+    def  __init__(self, anzahl_räder, farbe, hersteller):
+        self.anzahl_räder= anzahl_räder
+        self.farbe = farbe
+        self.hersteller = hersteller
+
+
+
+auto1 = Auto(4, "rot", "Audi")
+auto2 = Auto(4, "blau", "BMW")
+auto3 = Auto(4, "gruen", "Tesla")
+auto4 = Auto(4, "grau", "Tesla")
+
+print(auto1.hersteller)
+print(auto2.farbe)
